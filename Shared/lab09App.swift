c723085d3db83ad26e3dd1dd32c9c3d4c576f1fd
Brandon <brandon.se2009@gmail.com>
2021-10-11 11:09:15 -0400

    primer commit

---
+++ Shared/lab09App.swift
@@ -0,0 +1,17 @@
+//
+//  lab09App.swift
+//  Shared
+//
+//  Created by user204403 on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct lab09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
